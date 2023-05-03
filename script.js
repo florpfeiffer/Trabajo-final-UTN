95ec8a88ea73297bee82d182883562bc93cf722a
Florencia Pfeiffer <104425503+florpfeiffer@users.noreply.github.com>
2023-05-03 02:44:49 -0300

    casi listo

--- script.js
+++ script.js
@@ -11,16 +11,23 @@ const reproducir=()=>{
     let video=document.getElementsByClassName("video")[0];
 
     video.play();
-    if(video.duration < 60){
         let duracionVideo=video.duration.toFixed(0);
+        console.log(video.duration.toFixed(0))
 
-        document.querySelector("#duracion").textContent=`00: ${duracionVideo}`
-    }
-    mostrarMin()
-
+        document.querySelector("#duracion").textContent=`00:${duracionVideo}`
+    
+    
+    setInterval(()=> {
+        if (video.currentTime < 60) {
+            let reproduciendo=video.currentTime.toFixed(0);
+            document.querySelector("#reproduciendo").textContent =`00:${reproduciendo}`
+        }
+        
+    },1000)
 }
 
 const pausar=()=>{
     let video=document.querySelector(".video");
     video.pause();
 }
+
